db734c4eb319b101cbd5176da4f0de8962944132
D1msn <Flash.volkov@gmail.com>
2021-10-12 16:19:34 +0300

    init commit

---
+++ src/p2-homeworks/h1/Message.tsx
@@ -0,0 +1,13 @@
+import React from 'react'
+
+function Message() {
+    return (
+        <div>
+            <div className={"message__avatar message-avatar"} >
+                <img className={"message-avatar__img"} alt={"AVATAR"}/>
+            </div>
+        </div>
+    )
+}
+
+export default Message
